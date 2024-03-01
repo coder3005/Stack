d1bdd26832ab37f4dd83080d49839e9755a3d741
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-03-02 00:25:11 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -50,3 +50,54 @@ public class StackB {
         System.out.println("Is Palindrome: " + isPalindrome(head)); // Output: true
     }
 }
+
+/* another code of this 
+
+//palindrome Linked list
+
+import java.util.Stack;
+
+public class StackB {
+    public static class Node {
+        char data;
+        Node next;
+
+        public Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public static boolean isPalindrome(Node head) {
+        Stack<Character> stack = new Stack<>();
+        Node temp=head;
+        while(temp!=null) {
+            stack.push(temp.data);
+            temp=temp.next;
+        }
+        temp=head;
+        while(stack.size()>0) {
+            if(temp.data!=stack.pop()) {
+                return false;
+            }
+            else {
+                temp=temp.next;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        Node head = new Node('a');
+        head.next = new Node('b');
+        head.next.next = new Node('c');
+        head.next.next.next = new Node('b');
+        head.next.next.next.next = new Node('a');
+
+        System.out.println("Is Palindrome: " + isPalindrome(head)); // Output: true
+    }
+}
+*/
